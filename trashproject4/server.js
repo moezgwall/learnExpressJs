bb61f102b05d5f6f51386080be2690739b3af092
gWall <chroufa.moezkhan@gmail.com>
2025-10-06 04:53:39 +0200

    fixing minor issue

--- trashproject4/server.js
+++ trashproject4/server.js
@@ -7,6 +7,9 @@ const usersRouter = require('./routes/user');
 const errorHandler = require('./middleware/errorHandling');
 
 const app = express();
+app.use(express.json());
+
+
 const PORT = process.env.PORT || 3000;
 
 
