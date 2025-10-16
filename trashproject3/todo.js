7794cc07160a8aaab05727899e0cf71a468ece83
moez chroufa <chroufa.moezkhan@gmail.com>
2025-10-16 05:44:42 +0200

    adding a ui to the todo list

--- trashproject3/todo.js
+++ trashproject3/todo.js
@@ -1,65 +1,72 @@
-const express = require("express"); 
-const app = express(); 
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const PORT = 3000;
 // INIT OUR APP (expressJS)
-app.use(express.json());  
+app.use(express.json());
+app.use(cors());
 
 let todos = [
-	{id: 1,  task:"copy",completed:false}, 
-	{id:2,  task:"paste",completed:false},
-	{id:3,  task:"run",completed:false},
-]
+  { id: 1, task: "copy", completed: false },
+  { id: 2, task: "paste", completed: false },
+  { id: 3, task: "run", completed: false },
+];
 
-app.get("/api/todos", (req,res)=>{
-	res.json(todos); // orginial version
+app.get("/api/todos", (req, res) => {
+  res.json(todos); // orginial version
 });
 
-
-app.get("/api/todos/:id", (req,res)=>{
-	const {id} = req.params; // const id = req.params.id ; 
-	const todo = todos.find((t) => t.id === parseInt(id)); // verify that id exist 
-	if (!todo) return res.status(404).json({message:"To-Do not found"}); //  if it doesnt : server will return code : 404 (not found) with a message : 
-	res.json(todo);
+app.get("/api/todos/:id", (req, res) => {
+  const { id } = req.params; // const id = req.params.id ;
+  const todo = todos.find((t) => t.id === parseInt(id)); // verify that id exist
+  if (!todo) return res.status(404).json({ message: "To-Do not found" }); //  if it doesnt : server will return code : 404 (not found) with a message :
+  res.json(todo);
 });
 
-// req => { header :  " " , body : " " } 
+// req => { header :  " " , body : " " }
 
-app.post("/api/todos",(req,res)=>{
-	const newTask = req.body.task; // const {newTask} = req.body
-	const newState = req.body.completed; // const {newState} = req.body
-	if (!newTask){
-		return res.status(404).json({error: "task is required"});
-	}
-	if(!newState){
-		return res.status(404).json({error:"state is required"});
-	}
-	const newId = todos.length > 0 ? Math.max(...todos.map((todo)=> todo.id)) +1 : 1;
-	const newTodo = { id: newId, task:newTask,completed:newState}; 
-	todos.push(newTodo); 
+app.post("/api/todos", (req, res) => {
+  const { task, completed } = req.body;
+  if (typeof task !== "string" && task === "") {
+    return res
+      .status(400)
+      .json({ error: "task is required and must be a string" });
+  }
 
-	res.status(201).json(newTodo);
+  if (typeof completed !== "boolean") {
+    return res
+      .status(400)
+      .json({ error: "completed is required and must be a boolean" });
+  }
+
+  const newId =
+    todos.length > 0 ? Math.max(...todos.map((todo) => todo.id)) + 1 : 1;
+  const newTodo = { id: newId, task: task, completed: completed };
+  todos.push(newTodo);
+
+  res.status(201).json({ message: "task added with success!" });
+  //res.status(201).json(newTodo);
 });
 
-app.put("/api/todos/:id", (req,res) =>{
-	const {id} = req.params;
-	const {task,completed} = req.body;
+app.put("/api/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const { task, completed } = req.body;
 
-	const todo = todos.find((t) => t.id === parseInt(id)); 
-	if (!todo) return res.status(404).json({message: "todo not found"});
-	if (task) todo.task = task;
-	if (completed) todo.completed = completed;
-	res.json(todo);
-}); 
-
-app.delete("api/todos/:id", (req,res)=>{
-	const {id} = req.params;
-	const index = todos.findIndex((t)=> t.id === parseInt(id)); 
-	if (index ===1) return res.status(404).json({message:"todo not found"});
-	const deleteTodo = todos.splice(index,1);
-	res.json(deleteTodo[0]);
+  const todo = todos.find((t) => t.id === parseInt(id));
+  if (!todo) return res.status(404).json({ message: "todo not found" });
+  if (task) todo.task = task;
+  if (completed) todo.completed = completed;
+  res.json(todo);
 });
 
+app.delete("/api/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const index = todos.findIndex((t) => t.id === parseInt(id));
+  if (index === -1) return res.status(404).json({ message: "todo not found" });
+  const deleteTodo = todos.splice(index, 1);
+  res.json(deleteTodo[0]);
+});
 
-app.listen(PORT, ()=>{
-	console.log(`Server is running at http://localhost:${PORT}/api/todos`)
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}/api/todos`);
 });
