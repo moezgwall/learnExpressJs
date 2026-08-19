5fb4b2832f59c241fc5b39308c6bb9bf18bd5c75
moez chroufa <chroufa.moezkhan@gmail.com>
2026-08-19 05:52:08 +0100

    Ready set GO

--- typeScript/timer/main/index.ts
+++ typeScript/timer/main/index.ts
@@ -1,13 +1,26 @@
 
 import { timeSpan } from '../src/index';
 
-const timer = new timeSpan(0, 0, 30, 'timer-display');
+let timer: timeSpan | null = null;
 
 document.getElementById('start-btn')?.addEventListener('click', () => {
+    const HH = document.getElementById('hours') as HTMLInputElement;
+    const MM = document.getElementById('mints') as HTMLInputElement;
+    const SS = document.getElementById('secs') as HTMLInputElement;
+
+    const vhh = Number(HH.value) || 0;
+    const vmm = Number(MM.value) || 0;
+    const vss = Number(SS.value) || 0;
+
+    if (timer) {
+        timer.stop();
+    }
+    timer = new timeSpan(vhh, vmm, vss, 'timer-display');
+
     timer.startTime();
 });
 
 document.getElementById('stop-btn')?.addEventListener('click', () => {
-    timer.stop();
+    timer?.stop();
 });
 
