7a3de75ad2b432bbfcc0fa96c32e3b6f7cf76dfe
gWall <chroufa.moezkhan@gmail.com>
2026-09-12 21:19:39 +0100

    Fix typo in console log message

--- trashproject2/adduser.js
+++ trashproject2/adduser.js
@@ -9,7 +9,6 @@ app.use(express.json());
 const uri = "";
 const client = new MongoClient(uri);
 
-// the post middleware
 app.post('/adding-user', async (req,res)=>{
 
     const { id,fullname,email,img} = req.body;
@@ -18,7 +17,6 @@ app.post('/adding-user', async (req,res)=>{
         return res.status(400).json({error : "not found..."});
 
     }
-    // check if the user already exist in database
     try{    
         const db = client.db('mydb');
         const users = db.collection('USERS');
@@ -32,8 +30,6 @@ app.post('/adding-user', async (req,res)=>{
         await users.insertOne({ id,fullname,email,img} );
         res.status(201).json({message:"added succc"});
 
-
-
     }catch(err){
         console.error(err);
         res.status(500).json({error : "ur mom"});
@@ -43,7 +39,7 @@ app.post('/adding-user', async (req,res)=>{
 app.listen(PORT, async ()=>{
     try{
         await client.connect();
-        console.log(`Server is sucking at http://localhost:${PORT}`);
+        console.log(`Server is running at http://localhost:${PORT}`);
     }catch(err){
         console.log("failed to connect to db", err);
     }
